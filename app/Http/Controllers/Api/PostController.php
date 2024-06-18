f816e3d034235cfeaea9a915ea2fe03632293647
fauzanmzlm <fauzanmazlam88@gmail.com>
2024-06-18 14:03:33 +0800

    Displaying data detail by using ID

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -66,4 +66,19 @@ class PostController extends Controller
         //return response
         return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
     }
+
+    /**
+     * show
+     *
+     * @param  mixed $id
+     * @return void
+     */
+    public function show($id)
+    {
+        //find post by ID
+        $post = Post::find($id);
+
+        //return single post as a resource
+        return new PostResource(true, 'Detail Data Post!', $post);
+    }
 }
